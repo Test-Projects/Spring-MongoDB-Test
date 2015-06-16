475573b3d7e1203fe41efe07b280ef9fc6e3fd15
Nazar Oprysk <CycleDev@gmail.com>
2015-06-17 00:02:53 +0300

    setup application properties

--- src/main/java/nao/cycledev/springmongodb/config/MongoDBConfig.java
+++ src/main/java/nao/cycledev/springmongodb/config/MongoDBConfig.java
@@ -4,7 +4,6 @@ import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
